8cedf930a5a8583dc1c603cd35321b4be02971e2
KZ | Quoc Luong <likuroquoc@gmail.com>
2026-06-18 14:36:39 +0700

    update

--- src/utils/xrStore.ts
+++ src/utils/xrStore.ts
@@ -2,6 +2,7 @@ import { createXRStore } from "@react-three/xr";
 
 // Configure WebXR store with hand tracking and controller support
 export const xrStore = createXRStore({
+  offerSession: "immersive-vr",
   controller: true,
   hand: {
     left: true,
